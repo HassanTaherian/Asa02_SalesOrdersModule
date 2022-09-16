1c31fe6e209a7231af04d0ae49fe4db74aec600b
Celtiii <celtiii154@gmail.com>
2022-09-16 21:52:53 +0430

    WishListEntity

---
+++ Domain/Entities/WishList.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class WishList
+    {
+        public int Id { get; set; }
+
+        public long UserId { get; set; }
+
+        public virtual ICollection<WishListItem>? WishListItems { get; set; }
+    }
+}
---
+++ Domain/Entities/WishListItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public abstract class WishListItem : BaseEntity
+    {
+        public long ProductId { get; set; }
+
+        public virtual WishList WishList { get; set; }
+    }
+}
