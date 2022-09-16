16cf39e1ade3e96a60ec3d122f3407e10daf6775
Celtiii <celtiii154@gmail.com>
2022-09-17 01:39:47 +0430

    Add Db Context

---
+++ Presistance/Repositories/RepositoryDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Repositories
+{
+    public class RepositoryDbContext : DbContext
+    {
+        public RepositoryDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public virtual DbSet<Cart> Carts { get; set; }
+        public virtual DbSet<CartItem> CartItems { get; set; }
+        public virtual DbSet<Invoice> Invoices { get; set; }
+        public virtual DbSet<InvoiceItem> InvoiceItems { get; set; }
+        public virtual DbSet<WishList> WishLists { get; set; }
+        public virtual DbSet<WishListItem> WishListItems { get; set; }
+        public virtual DbSet<BaseEntity> BaseEntities { get; set; }
+    }
+}
