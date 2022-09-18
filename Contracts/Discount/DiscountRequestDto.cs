ab43eb055095099107cdda9c6aca3a21cb98dce1
HassanTSF <hassantsf.acd@gmail.com>
2022-09-18 22:19:38 +0430

    Adding Discount Module Dto's

---
+++ Contracts/Discount/DiscountRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Discount
+{
+    public class DiscountRequestDto
+    {
+        public int UserId { get; set; }
+
+        public int TotalPrice { get; set; }
+
+        public string DiscountCode { get; set; }
+
+        public ICollection<DiscountProductRequestDto>? Products { get; set; }
+    }
+}
