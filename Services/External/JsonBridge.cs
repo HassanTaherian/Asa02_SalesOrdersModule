e5c461d8301640c053f93b4c97a651b4686df17e
HassanTSF <hassantsf.acd@gmail.com>
2022-09-20 17:22:57 +0430

    Add JsonConvertor and HttpProvider

---
+++ Services/External/JsonBridge.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace WebClientTest
+{
+    public class JsonBridge<T>
+    {
+        public string Serialize(T item)
+        {
+            return JsonConvert.SerializeObject(item);
+        }
+
+        public string SerializeList(ICollection<T> items)
+        {
+            return JsonConvert.SerializeObject(items);
+        }
+
+        public ICollection<T>? DeserializeList(string json)
+        {
+            return JsonConvert.DeserializeObject<ICollection<T>>(json);
+        }
+
+        public T? Deserialize(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
