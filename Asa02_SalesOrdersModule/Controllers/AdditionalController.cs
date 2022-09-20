ead328d97798e6b000097b6157b593500221f472
Celtiii <celtiii154@gmail.com>
2022-09-20 12:12:38 +0430

    Define Discount & Address Controller

---
+++ Asa02_SalesOrdersModule/Controllers/AdditionalController.cs
@@ -0,0 +1,38 @@
+﻿using Contracts.UI;
+using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions;
+
+namespace Asa02_SalesOrdersModule.Controllers
+{
+    [ApiController, Route("/OrdersAdditional")]
+    public class AdditionalController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+        private readonly IAddressService _addressService;
+
+        public AdditionalController(IDiscountService discountService,
+            IAddressService addressService)
+        {
+            _discountService = discountService;
+            _addressService = addressService;
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> AddDiscountCode(
+            [FromBody] AdditionalInvoiceDataDto additionalInvoiceDataDto)
+        {
+            await _discountService.SetDiscountCodeAsync(additionalInvoiceDataDto);
+            return Ok("Successful");
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> AddAddressCode(
+            [FromBody] AdditionalInvoiceDataDto additionalInvoiceDataDto)
+        {
+            await _addressService.SetAddressIdAsync(additionalInvoiceDataDto);
+            return NoContent();
+        }
+    }
+
+}
+        
