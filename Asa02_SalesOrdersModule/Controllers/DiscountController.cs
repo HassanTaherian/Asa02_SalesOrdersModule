33ece35d771b87591a7d76f9bb0dd4015ce05a50
Celtiii <celtiii154@gmail.com>
2022-09-22 13:30:27 +0330

    seperate Address & Discount Controller

---
+++ Asa02_SalesOrdersModule/Controllers/DiscountController.cs
@@ -0,0 +1,26 @@
+﻿using Contracts.UI;
+using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions;
+
+namespace Asa02_SalesOrdersModule.Controllers
+{
+    [ApiController, Route("api/[controller]/[action]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+
+        public DiscountController(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> AddDiscountCode(
+            [FromBody] DiscountCodeRequestDto discountCodeRequestDto)
+        {
+            await _discountService.SetDiscountCodeAsync
+                (discountCodeRequestDto, CancellationToken.None);
+            return Ok("Successful");
+        }
+    }
+}
--- Asa02_SalesOrdersModule/Program.cs
+++ Asa02_SalesOrdersModule/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<IUnitOfWork , RepositoryDbContext>(options =>
+builder.Services.AddDbContext<IUnitOfWork, RepositoryDbContext>(options =>
     {
         options.UseSqlServer(
             builder.Configuration.GetConnectionString("Asa02SalesOrdersModule")
@@ -31,7 +31,6 @@ builder.Services.AddScoped<IDiscountService, DiscountService>();
 builder.Services.AddScoped<ISecondCartService, SecondCartService>();
 builder.Services.AddSingleton<IHttpProvider, HttpProvider>();
 // TODO: Inject HttpClient
-
 
 var app = builder.Build();
 
