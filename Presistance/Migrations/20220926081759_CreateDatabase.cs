7f6b7528276fadec1e8d09738da66b44a98ca2fd
HassanTSF <hassantsf.acd@gmail.com>
2022-09-26 12:02:37 +0330

    temp

---
+++ Presistance/Migrations/20220926081759_CreateDatabase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotlPrice",
+                table: "Invoices");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "TotlPrice",
+                table: "Invoices",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
